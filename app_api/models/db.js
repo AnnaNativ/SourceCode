e3128b4a1ac5d943620e5e39c4b5d9aba47a0d15
Ido Yaari <suzyido@yahoo.com>
2017-06-27 19:53:01 +0300

    Added system colleciton loading

--- app_api/models/db.js
+++ app_api/models/db.js
@@ -1,5 +1,9 @@
 var mongoose = require('mongoose');
+
 var gracefulShutdown;
+module.exports.systemConfig = {
+  
+};
 var dbURI = 'mongodb://localhost/meanAuth';
 if (process.env.NODE_ENV === 'production') {
   dbURI = process.env.MONGOLAB_URI;
@@ -10,6 +14,15 @@ mongoose.connect(dbURI);
 // CONNECTION EVENTS
 mongoose.connection.on('connected', function() {
   console.log('Mongoose connected to ' + dbURI);
+  // Get system data from the db
+  System.find({}, function(err, systemData){
+    if (err) {
+      throw err;
+    }
+    systemConfig = systemData[0];
+    console.log('Using verison: ' + systemData[0].version);
+    console.log('Pictures location: ' + systemData[0].imagesLocation);
+  });
 });
 mongoose.connection.on('error', function(err) {
   console.log('Mongoose connection error: ' + err);
@@ -49,5 +62,5 @@ process.on('SIGTERM', function() {
 require('./users');
 require('./exercises');
 require('./subjects');
-require('./subjects');
-require('./assignments');
+require('./system');
+var System = mongoose.model('System');
