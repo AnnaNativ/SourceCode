4a8a24ce5995bb5e81227b24fe70ce623d478ed3
AnnaNativ <anna_nativ@yahoo.com>
2017-07-03 16:06:14 +0300

    FIX

--- app_client/profile/profile.controller.js
+++ app_client/profile/profile.controller.js
@@ -1,72 +1,63 @@
-(function() {
-  
+(function () {
+
   angular
     .module('meanApp')
     .controller('profileCtrl', profileCtrl);
 
-  profileCtrl.$inject = ['$location', 'meanData','assignment'];
-  function profileCtrl($location, meanData,assignment) {
- 
-     var vm = this;
+  profileCtrl.$inject = ['$location', 'meanData', 'assignment'];
+  function profileCtrl($location, meanData, assignment) {
+
+    var vm = this;
 
     vm.user = {};
     vm.myAssignments;
     vm.selectedAssignment;
     vm.myProgress;
     vm.isInOriginalSubject = true;
-    
-    vm.isTeacher = function() {
+
+    vm.isTeacher = function () {
       return (vm.user.role == "teacher");
     }
-    vm.isStudent = function() {
+    vm.isStudent = function () {
       return (vm.user.role == "student");
     }
-    
+
     meanData.getProfile()
-      .success(function(data) {
+      .success(function (data) {
         vm.user = data;
         //console.log('Got user profile ' + JSON.stringify(data));
-        console.log('about to call for assignment for user '+ vm.user.name);
+        console.log('about to call for assignment for user ' + vm.user.name);
         assignment.myAssignments(vm.user)
-        .success(function(data){
-          console.log('got back with ' + data.length + ' assignments');
-          vm.myAssignments = data;
-        })
-        .error(function(e){
-          console.log(e);
-        });
+          .success(function (data) {
+            console.log('got back with ' + data.length + ' assignments');
+            vm.myAssignments = data;
+          })
+          .error(function (e) {
+            console.log(e);
+          });
 
       })
       .error(function (e) {
         console.log(e);
       });
 
-      vm.lastLocation = function(){
-        console.log('checking where you left it of in your previous session: '+ vm.selectedAssignment);
-        assignment.myLastLocation(vm.selectedAssignment)
-        .success(function(data){
-          console.log('your last subject was ' + JSON.stringify(data));
-          vm.myProgress = data;
-         /* if(data[0].subSubjectId != vm.selectedAssignment.subSubjectId){
-            console.log('You are not in your origianl subject');
-            vm.isInOriginalSubject = false;
-          }else{
-            console.log('You are in your original subject');
-            vm.isInOriginalSubject = true;
-          }
-          */
-          
+    vm.lastLocation = function () {
+      console.log('checking where you left it of in your previous session: ' + vm.selectedAssignment);
+      assignment.myLastLocation(vm.selectedAssignment)
+        .success(function (data) {
+          $location.path('profile');
+
         })
-        .error(function(e){
+        .error(function (e) {
           console.log(e);
-        });
-    };
+        })
+  };
 
-    vm.myInitialSubSubject = function(){
-      console.log('Going to load an exercise from the initial subject');
-    };
-        
+  vm.myInitialSubSubject = function () {
+    console.log('Going to load an exercise from the initial subject');
+  };
 
-  }
+
+}
 
 })();
