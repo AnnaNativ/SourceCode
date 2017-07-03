d4178ffb5c39ac5c20a80d2a678a80802569d002
Ido Yaari <suzyido@yahoo.com>
2017-07-03 10:15:43 +0300

    commit

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -6,27 +6,29 @@ var auth = jwt({
   userProperty: 'payload'
 });
 
+// Requires multiparty 
+multiparty = require('connect-multiparty');
+multipartyMiddleware = multiparty();
+
 var ctrlProfile = require('../controllers/profile');
 var ctrlAuth = require('../controllers/authentication');
 var ctrlExercise = require('../controllers/exercise');
 var ctrlSubjects = require('../controllers/subjects');
-var ctrlAssignment = require('../controllers/assignments');
+var ctrlUploads = require('../controllers/uploads');
 
 // profile
 router.get('/profile', auth, ctrlProfile.profileRead);
 router.get('/studentsOfTeacher', auth, ctrlProfile.getStudentsOfTeacher);
 router.get('/teachersList', ctrlProfile.getTeachersList);
+router.get('/schoolsList', ctrlProfile.getSchoolsList);
 
 // exercises
 router.get('/exercise', auth, ctrlExercise.getExercise);
-router.get('/exercisesForSubjectAndSubSubject', auth, ctrlExercise.getExercisesForSubjectAndSubSubject);
+router.get('/exercises', auth, ctrlExercise.getExercises);
 // subjects
 router.get('/subjects', auth, ctrlSubjects.getSubjects);
-
-//assignment
-router.get('/myAssignments', ctrlAssignment.getMyAssignments);
-router.get('/myLastLocation', ctrlAssignment.getMyLastLocation);
-
+router.get('/subSubjects', auth, ctrlSubjects.getSubSubjects);
+router.get('/videos', auth, ctrlSubjects.getVideos);
 
 
 // authentication
@@ -35,6 +37,9 @@ router.post('/login', ctrlAuth.login);
 router.post('/logout', ctrlAuth.logout);
 
 router.post('/exercise', ctrlExercise.newExercise);
-
+router.post('/subject', ctrlSubjects.newSubject);
+router.post('/subSubject', ctrlSubjects.newSubSubject);
+// pictures upload
+router.post('/upload', multipartyMiddleware, ctrlUploads.uploadPic);
 
 module.exports = router;
