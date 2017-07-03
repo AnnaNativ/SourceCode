acff3cd63376cefaa044c43db0d342cd538f7421
Ido Yaari <suzyido@yahoo.com>
2017-07-03 15:15:15 +0300

    Added validation to the login screen

--- app_client/auth/login/login.controller.js
+++ app_client/auth/login/login.controller.js
@@ -8,23 +8,28 @@
   function loginCtrl($location, authentication) {
     console.log('Login controller is running!!!');
     var vm = this;
+    vm.formValid = true;
 
     vm.credentials = {
       email : "",
       password : ""
     };
 
-    vm.onSubmit = function () {
-      authentication
-        .login(vm.credentials)
-        .error(function(err){
-          alert(err);
-        })
-        .then(function(){
-          $location.path('profile');
-        });
-    };
-
+    vm.submit = function(isValid) {
+      if (!isValid) {
+        vm.formValid = false;
+      }
+      else {
+        authentication
+          .login(vm.credentials)
+          .error(function(err){
+            alert(err);
+          })
+          .then(function(){
+            $location.path('profile');
+          });
+      };
+    }
   }
 
 })();
