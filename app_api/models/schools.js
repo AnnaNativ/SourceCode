f2a4432b1d0e170f9dd104ef01b46a0d0cc5e5d8
Ido Yaari <suzyido@yahoo.com>
2017-07-09 21:21:39 +0300

    Go back to older version

--- app_api/models/schools.js
+++
@@ -1,7 +0,0 @@
-//var mongoose = require( './db' );
-var mongoose = require('mongoose');
-
-var schoolsSchema = new mongoose.Schema({name:String});
-
-mongoose.model('School', schoolsSchema);
-
--- app_api/models/system.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-
-var systemSchema = new mongoose.Schema({
-  version: String,
-  imagesLocation: String
-},{ collection: 'system' });
-
-mongoose.model('System', systemSchema);
-
